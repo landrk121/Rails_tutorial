a0672f61a11ee1aae82e919074a0feed33198b0c
tnkrk121 <tanariku121.wolf12@gmail.com>
2018-11-25 09:17:54 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   resources :users
   post '/signup',  to: 'users#create'
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
